5418384bda49cb036745de8814cc1dc82784db0a
Jonathan Dahan <git@jonathan.is>
2023-10-13 15:49:16 -0400

    same tests for both styles

--- test.ts
+++ test.ts
@@ -2,8 +2,8 @@ import assert from 'node:assert'
 import test from 'node:test'
 import { config, PEN_LIFT_MECHANISM, SC, stepperAndServoModeConfigure, STEPPER_SIGNAL_CONTROL } from './commands'
 
-test('api styles', async (t) => {
-    await t.test('discriminatedUnion - pen lift', async () => {
+test('discriminatedUnion - pen lift', async (t) => {
+    await t.test('generates the correct command', async () => {
         assert.strictEqual(
             'SC,1,1\r', 
             stepperAndServoModeConfigure({
@@ -11,12 +11,17 @@ test('api styles', async (t) => {
                 integer: PEN_LIFT_MECHANISM.RC_SERVO_OUT
             })
         )
+    })
 
+    await t.test('errors on incorrect integer', () => {
         // @ts-expect-error
         stepperAndServoModeConfigure({parameter: SC.PEN_LIFT_MECHANISM, integer: 4})
     })
 
-    
+    await t.test('errors on compatible integer from incorrect enum', () => {
+        // @ts-expect-error
+        stepperAndServoModeConfigure({parameter: SC.PEN_LIFT_MECHANISM, integer: STEPPER_SIGNAL_CONTROL.DISCONNECT})
+    })
 })
 
 test('overload - pen lift', async (t) => {
