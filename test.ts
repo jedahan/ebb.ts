83d575a385064a6257fef097f6fb7985e8b451a9
Jonathan Dahan <git@jonathan.is>
2023-10-13 15:29:09 -0400

    try a different style

--- test.ts
+++ test.ts
@@ -1,10 +1,9 @@
 import assert from 'node:assert'
 import test from 'node:test'
-import { PEN_LIFT_MECHANISM, SC, stepperAndServoModeConfigure } from './commands'
+import { config, PEN_LIFT_MECHANISM, SC, stepperAndServoModeConfigure, STEPPER_SIGNAL_CONTROL } from './commands'
 
-test('stepperAndServoModeConfigure', (t) => {
-    t.test('pen lift', (t) => {
-        
+test('api styles', async (t) => {
+    await t.test('discriminatedUnion - pen lift', async () => {
         assert.strictEqual(
             'SC,1,1\r', 
             stepperAndServoModeConfigure({
@@ -13,7 +12,28 @@ test('stepperAndServoModeConfigure', (t) => {
             })
         )
 
-        /// @ts-expect-error
+        // @ts-expect-error
         stepperAndServoModeConfigure({parameter: SC.PEN_LIFT_MECHANISM, integer: 4})
+    })
+
+    
+})
+
+test('overload - pen lift', async (t) => {
+    await t.test('generates the correct command', () => {
+        assert.strictEqual(
+            'SC,1,1\r', 
+            config(SC.PEN_LIFT_MECHANISM, PEN_LIFT_MECHANISM.RC_SERVO_OUT)
+        )
+    })
+
+    await t.test('errors on incorrect integer', () => {
+        // @ts-expect-error
+        config(SC.PEN_LIFT_MECHANISM, 4)
+    })
+    
+    await t.test('errors on compatible integer from incorrect enum', () => {
+        // @ts-expect-error
+        config(SC.PEN_LIFT_MECHANISM, STEPPER_SIGNAL_CONTROL.DISCONNECT)
     })
 })
